0e73ccbaf0d0bb8666807eebb5135683f54856ec
JasonKChow <jason.k.chow@vanderbilt.edu>
2019-11-14 16:34:01 -0600

    localizer preprocessing batch

---
+++ localizer_preproc.m
@@ -0,0 +1,110 @@
+% List of open inputs
+% Slice Timing: Session - cfg_files
+% Slice Timing: Number of Slices - cfg_entry
+% Slice Timing: TA - cfg_entry
+% Slice Timing: Slice order - cfg_entry
+% Coregister: Estimate & Reslice: Reference Image - cfg_files
+% Normalise: Write: Deformation Field - cfg_files
+clear all; %#ok<CLALL>
+clc;
+
+% Base directory
+baseDir = '/Users/Wasabi/Experiments_Wasabi/Rankin/Magnum2/';
+% Get subject folders
+sbjs = dir([baseDir 'Subjects/0*']);
+
+% Calculate number of runs
+nrun = length(sbjs); 
+jobfile = {[baseDir 'localizer_preproc_job.m']};
+jobs = repmat(jobfile, 1, nrun);
+inputs = cell(6, nrun);
+toRemove = {};
+
+% Loop through participants
+for crun = 1:nrun
+    try 
+        % Get directories
+        sbjName = sbjs(crun).name;
+        sbjDir = [baseDir 'Subjects/' sbjName '/'];
+        strucFolder = 'structural/';
+        
+        % Change to other name if necessary
+        if ~exist([sbjDir '/' strucFolder], 'dir')
+            strucFolder = 'new_structural/';
+        end
+        
+        % Get localizer
+        localizer = [sbjDir sbjName '_loc.nii'];
+       
+        % Check if localizer exists
+        if ~isfile(localizer)
+            warning([sbjName ' is missing localizer file, skipping.']);
+            toRemove = [toRemove crun];  %#ok<*AGROW>
+            continue
+        end
+        
+        % Add structural scan for coregistration
+        structural = [sbjDir strucFolder sbjName '_SS_struct.nii'];
+        
+        % Check structural exists
+        if ~isfile(structural)
+            warning([sbjName ' is missing structural file, skipping.']);
+            toRemove = [toRemove crun]; 
+            continue
+        end
+        
+        % Add deformation file
+        defMap = [sbjDir strucFolder 'y_' sbjName '_SS_struct.nii'];
+        
+        % Check if deformation file exists
+        if ~isfile(defMap)
+            warning([sbjName ' is missing deformation map, skipping.']);
+            toRemove = [toRemove crun];
+            continue
+        end
+        
+        % Get localizer run info
+        tmp = niftiinfo(localizer);
+        
+        % Fill cell array with slices, note only 1 session is assumed
+        slices = strcat(repmat({[localizer ',']}, tmp.ImageSize(4), 1), ...
+            num2str([1:tmp.ImageSize(4)]')); %#ok<NBRAK>
+        slices = strrep(slices, ' ', '');
+
+        inputs{1, crun} = cellstr(slices);
+
+        % Add number of slices per session
+        inputs{2, crun} = tmp.ImageSize(3);
+        
+        % Add TA, assumes TR of 2
+        inputs{3, crun} = 2 - (2 / tmp.ImageSize(3));
+        
+        % Add slice order
+        inputs{4, crun} = [1:2:tmp.ImageSize(3) 2:2:tmp.ImageSize(3)];
+
+        % Add structural file
+        inputs{5, crun} = cellstr(structural);
+        
+        % TODO: Add Deformation field
+        inputs{6, crun} = cellstr(defMap);
+
+    catch
+        warning(['Subject ' sbjName ' skipped due to unknown error.']);
+        toRemove = [toRemove crun]; 
+    end
+end
+
+% Remove runs that failed to acquire inputs
+inputs(:, [toRemove{:}]) = [];
+jobs(:, [toRemove{:}]) = [];
+
+% Print
+fprintf(['Running batch on ' num2str(length(jobs)) ' subjects.\n']);
+
+% Run
+spm('defaults', 'FMRI');
+spm_jobman('run', jobs, inputs{:});
+
+% Print skipped subjects
+fprintf('Subjects skipped\n');
+fprintf('%s\n', sbjs([toRemove{:}], :).name);
---
+++ localizer_preproc_job.m
@@ -0,0 +1,48 @@
+%-----------------------------------------------------------------------
+% Job saved on 14-Nov-2019 13:25:07 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7487)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.temporal.st.scans = {'<UNDEFINED>'};
+matlabbatch{1}.spm.temporal.st.nslices = '<UNDEFINED>';
+matlabbatch{1}.spm.temporal.st.tr = 2;
+matlabbatch{1}.spm.temporal.st.ta = '<UNDEFINED>';
+matlabbatch{1}.spm.temporal.st.so = '<UNDEFINED>';
+matlabbatch{1}.spm.temporal.st.refslice = 2;
+matlabbatch{1}.spm.temporal.st.prefix = 'a';
+matlabbatch{2}.spm.spatial.realign.estwrite.data{1}(1) = cfg_dep('Slice Timing: Slice Timing Corr. Images (Sess 1)', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{1}, '.','files'));
+matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.quality = 0.9;
+matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.sep = 4;
+matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.fwhm = 5;
+matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.rtm = 1;
+matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.interp = 2;
+matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.wrap = [0 0 0];
+matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.weight = '';
+matlabbatch{2}.spm.spatial.realign.estwrite.roptions.which = [0 1];
+matlabbatch{2}.spm.spatial.realign.estwrite.roptions.interp = 4;
+matlabbatch{2}.spm.spatial.realign.estwrite.roptions.wrap = [0 0 0];
+matlabbatch{2}.spm.spatial.realign.estwrite.roptions.mask = 1;
+matlabbatch{2}.spm.spatial.realign.estwrite.roptions.prefix = 'r';
+matlabbatch{3}.spm.spatial.coreg.estwrite.ref = '<UNDEFINED>';
+matlabbatch{3}.spm.spatial.coreg.estwrite.source(1) = cfg_dep('Realign: Estimate & Reslice: Mean Image', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','rmean'));
+matlabbatch{3}.spm.spatial.coreg.estwrite.other(1) = cfg_dep('Realign: Estimate & Reslice: Realigned Images (Sess 1)', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','sess', '()',{1}, '.','cfiles'));
+matlabbatch{3}.spm.spatial.coreg.estwrite.eoptions.cost_fun = 'nmi';
+matlabbatch{3}.spm.spatial.coreg.estwrite.eoptions.sep = [4 2];
+matlabbatch{3}.spm.spatial.coreg.estwrite.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+matlabbatch{3}.spm.spatial.coreg.estwrite.eoptions.fwhm = [7 7];
+matlabbatch{3}.spm.spatial.coreg.estwrite.roptions.interp = 4;
+matlabbatch{3}.spm.spatial.coreg.estwrite.roptions.wrap = [0 0 0];
+matlabbatch{3}.spm.spatial.coreg.estwrite.roptions.mask = 0;
+matlabbatch{3}.spm.spatial.coreg.estwrite.roptions.prefix = 'r';
+matlabbatch{4}.spm.spatial.normalise.write.subj.def = '<UNDEFINED>';
+matlabbatch{4}.spm.spatial.normalise.write.subj.resample(1) = cfg_dep('Coregister: Estimate & Reslice: Coregistered Images', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','cfiles'));
+matlabbatch{4}.spm.spatial.normalise.write.woptions.bb = [-78 -112 -70
+                                                          78 76 85];
+matlabbatch{4}.spm.spatial.normalise.write.woptions.vox = [2 2 2];
+matlabbatch{4}.spm.spatial.normalise.write.woptions.interp = 4;
+matlabbatch{4}.spm.spatial.normalise.write.woptions.prefix = 'w';
+matlabbatch{5}.spm.spatial.smooth.data(1) = cfg_dep('Normalise: Write: Normalised Images (Subj 1)', substruct('.','val', '{}',{4}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{1}, '.','files'));
+matlabbatch{5}.spm.spatial.smooth.fwhm = [6 6 6];
+matlabbatch{5}.spm.spatial.smooth.dtype = 0;
+matlabbatch{5}.spm.spatial.smooth.im = 0;
+matlabbatch{5}.spm.spatial.smooth.prefix = 's';
