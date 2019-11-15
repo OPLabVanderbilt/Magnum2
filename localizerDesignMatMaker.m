083bb10ef47541ec38a15a457ebaad57982e5792
JasonKChow <jason.k.chow@vanderbilt.edu>
2019-11-14 18:29:42 -0600

    batch localizer design matrix maker

---
+++ localizerDesignMatMaker.m
@@ -0,0 +1,106 @@
+% List of open inputs
+% fMRI model specification: Directory - cfg_files
+% fMRI model specification: Scans - cfg_files
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+clear all; %#ok<CLALL>
+clc;
+
+% Base directory
+baseDir = '/Users/Wasabi/Experiments_Wasabi/Rankin/Magnum2/';
+% Get subject folders
+sbjs = dir([baseDir 'Subjects/0*']);
+
+% Calculate number of runs
+nrun = length(sbjs); 
+jobfile = {[baseDir 'localizerDesignMatMaker_job.m']};
+jobs = repmat(jobfile, 1, nrun);
+inputs = cell(6, nrun);
+toRemove = {};
+
+for crun = 1:nrun
+    try
+        % Get directories
+        sbjName = sbjs(crun).name;
+        sbjDir = [baseDir 'Subjects/' sbjName '/'];
+        
+        % Get preprocessed localizer file
+        locFile = [sbjDir 'swa' sbjName '_loc.nii'];
+        
+        % Check if localizer exists
+        if ~isfile(locFile)
+            warning([sbjName ' is missing preprocessed localizer file, ' ...
+                'skipping.']);
+            toRemove = [toRemove crun];  %#ok<*AGROW>
+            continue
+        end
+        
+        % Get trial file
+        trialFile = [baseDir 'Subjects/s' sbjName(1:3) '_r1_' ...
+            sbjName(4:numel(sbjName)) '_ProjImagLoc.mat'];
+        
+        % Check if trial file exists
+        if ~isfile(trialFile)
+            warning([sbjName ' is missing trial file, skipping.']);
+            toRemove = [toRemove crun];
+            continue
+        end
+        
+        % Set directory for design matrix
+        inputs{1, crun} = cellstr(sbjDir);
+        
+        % Get localizer run info
+        tmp = niftiinfo(locFile);
+        
+        % Fill cell array with slices, note only 1 session is assumed
+        slices = strcat(repmat({[locFile ',']}, tmp.ImageSize(4), 1), ...
+            num2str([1:tmp.ImageSize(4)]')); %#ok<NBRAK>
+        slices = strrep(slices, ' ', '');
+
+        % Set scans
+        inputs{2, crun} = cellstr(slices);
+        
+        % Load trial file
+        load(trialFile)
+        trialData = struct2table(trialInfo.trial);
+        
+        % Get block info
+        blockChange = arrayfun(@(x) find(trialData.blknum == x, 1), ...
+            unique(trialData.blknum));
+        blockCat = trialData.cat(blockChange);
+        blockOnset = floor(blockChange/2);
+        
+        % Populate Face onsets
+        inputs{3, crun} = blockOnset(contains(blockCat, 'F'))';
+        
+        % Populate Object onsets
+        inputs{4, crun} = blockOnset(contains(blockCat, 'O'))';
+        
+        % Populate Body onsets
+        inputs{5, crun} = blockOnset(contains(blockCat, 'B'))';
+        
+        % Populate Scrambled onsets
+        inputs{6, crun} = blockOnset(contains(blockCat, 'S'))';
+
+    catch
+        warning(['Subject ' sbjName ' skipped due to unknown error.']);
+        toRemove = [toRemove crun]; 
+    end
+end
+
+% Remove runs that failed to acquire inputs
+inputs(:, [toRemove{:}]) = [];
+jobs(:, [toRemove{:}]) = [];
+
+% Print
+fprintf(['Running batch on ' num2str(length(jobs)) ' subjects.\n']);
+
+% Run
+spm('defaults', 'FMRI');
+spm_jobman('run', jobs, inputs{:});
+
+% Print skipped subjects
+fprintf('Subjects skipped\n');
+fprintf('%s\n', sbjs([toRemove{:}], :).name);
---
+++ localizerDesignMatMaker_job.m
@@ -0,0 +1,49 @@
+%-----------------------------------------------------------------------
+% Job saved on 14-Nov-2019 16:57:51 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7487)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.fmri_spec.dir = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.fmri_spec.timing.units = 'scans';
+matlabbatch{1}.spm.stats.fmri_spec.timing.RT = 2;
+matlabbatch{1}.spm.stats.fmri_spec.timing.fmri_t = 45;
+matlabbatch{1}.spm.stats.fmri_spec.timing.fmri_t0 = 1;
+matlabbatch{1}.spm.stats.fmri_spec.sess.scans = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).name = 'Face';
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).onset = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).duration = 8;
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).tmod = 0;
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).pmod = struct('name', {}, 'param', {}, 'poly', {});
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).orth = 1;
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).name = 'Object';
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).onset = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).duration = 8;
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).tmod = 0;
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).pmod = struct('name', {}, 'param', {}, 'poly', {});
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).orth = 1;
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(3).name = 'BodyParts';
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(3).onset = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(3).duration = 8;
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(3).tmod = 0;
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(3).pmod = struct('name', {}, 'param', {}, 'poly', {});
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(3).orth = 1;
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(4).name = 'Scrambled';
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(4).onset = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(4).duration = 8;
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(4).tmod = 0;
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(4).pmod = struct('name', {}, 'param', {}, 'poly', {});
+matlabbatch{1}.spm.stats.fmri_spec.sess.cond(4).orth = 1;
+matlabbatch{1}.spm.stats.fmri_spec.sess.multi = {''};
+matlabbatch{1}.spm.stats.fmri_spec.sess.regress = struct('name', {}, 'val', {});
+matlabbatch{1}.spm.stats.fmri_spec.sess.multi_reg = {''};
+matlabbatch{1}.spm.stats.fmri_spec.sess.hpf = 128;
+matlabbatch{1}.spm.stats.fmri_spec.fact = struct('name', {}, 'levels', {});
+matlabbatch{1}.spm.stats.fmri_spec.bases.hrf.derivs = [0 0];
+matlabbatch{1}.spm.stats.fmri_spec.volt = 1;
+matlabbatch{1}.spm.stats.fmri_spec.global = 'None';
+matlabbatch{1}.spm.stats.fmri_spec.mthresh = 0.8;
+matlabbatch{1}.spm.stats.fmri_spec.mask = {''};
+matlabbatch{1}.spm.stats.fmri_spec.cvi = 'AR(1)';
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1) = cfg_dep('fMRI model specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
